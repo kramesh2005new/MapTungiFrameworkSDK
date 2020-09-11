291dd6517bb7bc241c7a351198afc8bea74e4985
Ramesh K <ramesh.kannan@smartzi.com>
2020-09-11 08:04:53 +0530

    Initial Commit

---
+++ MapTungiFramework/MapTungiFramework.h
@@ -0,0 +1,19 @@
+//
+//  MapTungiFramework.h
+//  MapTungiFramework
+//
+//  Created by Ramesh K on 11/09/20.
+//  Copyright © 2020 Ramesh. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for MapTungiFramework.
+FOUNDATION_EXPORT double MapTungiFrameworkVersionNumber;
+
+//! Project version string for MapTungiFramework.
+FOUNDATION_EXPORT const unsigned char MapTungiFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MapTungiFramework/PublicHeader.h>
+
+
